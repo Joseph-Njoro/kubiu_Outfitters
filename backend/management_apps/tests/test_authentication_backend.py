20147c3a9003aff8ef2ed40d790090135346507a
Joseph Njoroge <139031556+Joseph-Njoro@users.noreply.github.com>
2024-08-25 13:37:44 +0300

    apps.py passed tests

--- backend/management_apps/tests/test_authentication_backend.py
+++ backend/management_apps/tests/test_authentication_backend.py
@@ -1,27 +1,27 @@
 from django.test import TestCase
-from management_apps.authentication_backend import CustomBackend
+from management_apps.authentication_backend import EmailBackend
 from management_apps.models import CustomUser
 
 class TestAuthenticationBackend(TestCase):
     def setUp(self):
-        self.user = CustomUser.objects.create_user(username='testuser', password='12345')
+        self.user = CustomUser.objects.create_user(username='testuser', password='12345', email='test@example.com')
 
     def test_authenticate_valid_user(self):
-        backend = CustomBackend()
-        user = backend.authenticate(username='testuser', password='12345')
+        backend = EmailBackend()
+        user = backend.authenticate(request=None, email='test@example.com', password='12345')
         self.assertEqual(user, self.user)
 
     def test_authenticate_invalid_user(self):
-        backend = CustomBackend()
-        user = backend.authenticate(username='invaliduser', password='wrongpassword')
+        backend = EmailBackend()
+        user = backend.authenticate(request=None, email='invalid@example.com', password='wrongpassword')
         self.assertIsNone(user)
 
     def test_get_user(self):
-        backend = CustomBackend()
+        backend = EmailBackend()
         user = backend.get_user(self.user.pk)
         self.assertEqual(user, self.user)
 
     def test_get_user_invalid_id(self):
-        backend = CustomBackend()
+        backend = EmailBackend()
         user = backend.get_user(999)  # Assuming 999 is an invalid ID
         self.assertIsNone(user)
